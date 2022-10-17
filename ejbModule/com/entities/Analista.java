b47559a32d0e8864e072cefc019c39afc009d983
Jean Marshall <jean.marshall@estudiantes.utec.edu.uy>
2022-10-17 18:59:14 -0300

    Pruebas con git

--- ejbModule/com/entities/Analista.java
+++ ejbModule/com/entities/Analista.java
@@ -2,15 +2,23 @@ package com.entities;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * Entity implementation class for Entity: Analista
  *
  */
+@Entity
 public class Analista implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;	
+	
+	@Id
+	@GeneratedValue
+	private Long id;
 	public Analista() {
 		super();
 	} 
