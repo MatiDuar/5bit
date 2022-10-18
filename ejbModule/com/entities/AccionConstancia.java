d4cb22198a1c5dadb4467335bb15be4f67ede9db
Jean Marshall <jean.marshall@estudiantes.utec.edu.uy>
2022-10-18 20:57:03 -0300

    Correciones finales de entidades JPA

--- ejbModule/com/entities/AccionConstancia.java
+++ ejbModule/com/entities/AccionConstancia.java
@@ -14,7 +14,7 @@ public class AccionConstancia implements Serializable {
 	private static final long serialVersionUID = 1L;	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long ida;
+	private Long id;
 	
 	@Column(nullable=false)
 	private Date fechaHora;
